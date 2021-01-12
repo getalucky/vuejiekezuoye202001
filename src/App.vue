5624c284c4cf2cba05cc4295b6eae37cd2d45f52
Liaotyr <1318931707@qq.com>
2021-01-12 16:07:49 +0800

    底部栏组件化

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <template>
   <div id="app">
     <router-view/>
+    <bottab></bottab>
   </div>
 </template>
+
+<script>
+import bottab from './components/BotTab.vue'
+export default {
+  components:{
+    bottab
+  }
+}
+</script>
 
 <style lang="less">
 *{
---
+++ src/components/BotTab.vue
@@ -0,0 +1,35 @@
+<template>
+  <mt-tabbar v-model="selected">
+    <mt-tab-item id="1">
+      <img slot="icon" src="../assets/icon_agotahjb768/xinwen.png" />
+      新闻
+    </mt-tab-item>
+    <mt-tab-item id="2">
+      <img slot="icon" src="../assets/icon_agotahjb768/youwei.png" />
+      油价
+    </mt-tab-item>
+    <mt-tab-item id="3">
+      <img slot="icon" src="../assets/icon_agotahjb768/tianqi.png" />
+      天气预报
+    </mt-tab-item>
+  </mt-tabbar>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      selected: "",
+    };
+  },
+  watch: {
+    selected() {
+      console.log(this.selected);
+    },
+  },
+  components: {},
+};
+</script>
+
+<style>
+</style>
