9ee68d3577681336a49586e0d74868910d0c6eac
Liaotyr <1318931707@qq.com>
2021-01-12 15:29:25 +0800

    底部栏完成

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import flexible from 'lib-flexible'
 
 Vue.config.productionTip = false
+Vue.use(flexible);
+Vue.use(MintUI);
 
 new Vue({
   router,
